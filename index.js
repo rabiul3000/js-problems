7c48db408cd8a3882310384de035c5ef38ec53a1
rabiul3000 <mdrabiul.asia@gmail.com>
2025-02-16 11:48:22 +0600

    task 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('git initialized')
+// task 1 : Find the lowest number in the array below.
+
+function taskOne(heights2) {
+	let res = heights2[0];
+
+	for (number of heights2) {
+		if (number < res) {
+			res = number;
+		}
+	}
+    return res;
+}
+
+const task1Solution = taskOne([167, 190, 120, 165, 5]);
+console.log(task1Solution)
+
+
